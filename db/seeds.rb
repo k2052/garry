738efbb5ea8573a91fa63512862abf8329edeec0
Ken Erickson <bookworm.productions@gmail.com>
2012-04-16 21:36:42 -0700

    db:seeds for Account

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+require 'ffaker'
+
+shell.say "Create some accounts. 10 to be exact"
+
+10.times do |i|    
+  account = Account.new(:email => Faker::Internet.email, :username => Faker::Internet.user_name, :name => Faker::Name.name, :password => 'testpass', 
+    :password_confirmation => 'testpass')    
+  account.save 
+       
+  account = Account.find_by_id(account.id)
+  cu = Stripe::Customer.retrieve(account.stripe_id)   
+  card = {
+    :number    => 4242424242424242,
+    :exp_month => 8,
+    :exp_year  => 2013
+  } 
+  cu.card = card
+  cu.save
+end
