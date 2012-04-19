fe0317486c545cc1a6412cf389d6ab14bdaed262
Ken Erickson <bookworm.productions@gmail.com>
2012-04-19 11:56:47 -0700

    addedd cart.item_in_cart?

--- lib/garry/models/Cart.rb
+++ lib/garry/models/Cart.rb
@@ -18,8 +18,17 @@ module Garry
         item = CartItem.new(:type => item.class, :price => item.price, :product_id => item.id)   
       end
     
-      self.items << item
-    end  
+      self.items    << item      
+      self.item_ids << item.id.to_s
+    end 
+    
+    def item_in_cart?(item)    
+      if item.is_a?(String)
+        self.item_ids.include?(item)    
+      else          
+        self.item_ids.include?(item.id.to_s)    
+      end
+    end 
   
     def remove(item) 
       self.items.delete_if {|i| i.id == item.id}         
@@ -49,8 +58,7 @@ module Garry
     
     # Its probably best to overide this so you get correct objects. Instead of GProduct's
     def items_full(query={})    
-      product_ids = self.items.map { |i| i.product_id }   
-      GProduct.all({:id => product_ids}.merge!(query))
+      GProduct.all({:id => item_ids}.merge!(query))
     end
   end 
 end
